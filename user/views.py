68579f9d42b23c44a292bd79842170f2feeeb74d
evgeshakrasava <zplacesound@gmail.com>
2019-05-09 14:23:33 +0300

    last changes from master

--- user/views.py
+++ user/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from .forms import CustomUserRegistration
 
 def show_user_profile(request):
     return render(request,'user/profile.html')
@@ -8,13 +9,14 @@ def show_user_profile(request):
 def show_current_bought(request):
     pass
 def register(request):
-    if request.method =='POST':
-        if form.is_valid():
-            form.save()
-            username =  request.POST.get('username')
-            messages.success(request,f'Аккаунт  {username} был успешно создан')
-            return redirect('user_page')
-
-    form = UserCreationForm()
-    return render(request,'user/registration.html',{'title':'регистрация нового пользователя',
-                                                    'form':form })
+    if request.method == "POST":
+            form = CustomUserRegistration(request.POST)
+            if form.is_valid():
+                #take username from your request
+                form.save()
+                username =  request.POST.get('username')
+                messages.success(request,f'Аккаунт  {username} был успешно создан')
+                return redirect('home')
+    else:
+        form = CustomUserRegistration()
+    return render(request,'user/registration.html',{'form':form ,'title':'регистрация пользователя'})
